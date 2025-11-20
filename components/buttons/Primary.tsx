5ea37100358d7a0ff6d72a169516344febcf44c1
Craig Sampson <craigsjr15@gmail.com>
2025-11-19 23:17:27 -0500

    hover styles added to buttons

--- components/buttons/Primary.tsx
+++ components/buttons/Primary.tsx
@@ -2,7 +2,7 @@ import { buttonProps } from "@/lib/type";
 
 export default function Primary({ text, link }: buttonProps) {
   return (
-    <a href={link} target="_blank" className="bg-sage text-white button">
+    <a href={link} target="_blank" className="bg-sage text-white button hover:text-text transition duration-300 ease-in-out">
       {text}
     </a>
   );
--- components/buttons/Secondary.tsx
+++ components/buttons/Secondary.tsx
@@ -8,8 +8,8 @@ export default function Secondary({ text, link, color = true }: SecondaryProps) 
   return (
     <a
       href={link}
-      className={`bg-transparent border button ${
-        color ? "text-sage border-sage" : "text-white border-white"
+      className={`bg-transparent border button hover:text-text hover:border-text transition duration-300 ease-in-out ${
+        color ? "text-sage border-sage" : "text-white border-white "
       }`}
     >
       {text}
